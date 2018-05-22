c4f5444620aceb4e996338e4b0c0f683007b3cef
macgama <gael.manigley@gmail.com>
2018-05-22 21:47:11 +0200

    Mise à jour login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,39 +1,65 @@
 <template>
     <div class="container-fluid">
         <!-- The Modal Player -->
-        <div class="modalBox" v-if="error" style="color: red;">{{ $t(error.code) }}</div><br />
-            <form class="modal-content">
-            	<div class="container"> 
-               		<h1>Se connecter</h1>
-               		<p class="modalInscription">Va vite encourager tes équipes favorites !</p>
-               		<hr>
-					<label for="email"><b>Email</b></label>
-					<input type="text" v-model="email" placeholder="Enter Email" name="email" required>
+        <div id="modalBox">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <!-- Modal Header -->
+                    <div class="modal-header text-center">
+                        <span class="modal-title">Se connecter</span>
+                        <nuxt-link to="/">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true" class="white-text"><i class="fa fa-arrow-circle-left"></i></span>
+                            </button>
+                        </nuxt-link>                    
+                    </div>
+                    <!-- Modal body -->
+                    <div id="modalTitle" class="modal-body">
 
-					<label for="psw"><b>Password</b></label>
-					<input type="password" v-model="password" placeholder="Enter Password" name="psw" required>
+                        <br /><br />
+                        <div class="col-md-6 offset-md-6 text-center">
+                        <div v-if="error" class="text-center" style="color: red;">{{ $t(error.code) }}</div><br />
 
-					<label>
-					<input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Se souvenir de moi
-					</label>
+                        <form>
+                            <!-- <div class="group">      
+                                <input type="text" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label>Name</label>
+                            </div> -->
+                                
+                            <div class="group">      
+                                <input type="text" v-model="email" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label>Email</label>
+                            </div>
 
-					<div class="row button-section">
-					<div class="col-twelve">
-					<a href="index2.html" type="button" class="button stroke smoothscroll">Non, je ne veux pas !</a>
-					<a href="vue/load.html" type="submit" class="button button-primary">Go Go Go, c'est parti !</a>
-					</div>
-					</div>
+                            <div class="group">      
+                                <input type="password" v-model="password" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label>Password</label>
+                            </div>
 
-					<div class="row button-section">
-					<div class="col-twelve">
-					<a href="vue/load.html" type="submit" class="button btn-facebook"><i class="fa fa-facebook"></i> Connecte-toi avec FB</a>
-					<a href="vue/load.html" type="submit" class="button btn-twitter"><i class="fa fa-google"></i> Connecte-toi avec G+</a>
-					</div>
-					</div>
-           		</div>
-            </form>
-        </div>
-    </div>
+                            <!-- <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>   -->
+                            <button type="submit" class="btn btn-primary" style="" :disabled="loading" :loading="loading" @click.prevent="signUserIn"> Login <i v-bind:class="{'fa fa-spinner fa-spin' : loading}"></i></button>&nbsp;&nbsp;
+                            <br /><br />
+                        </form>
+                        </div>
+                    </div><!-- /.modal-body -->
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <nuxt-link to="/" type="button" class="btn btn-danger" data-dismiss="modal">Fermer</nuxt-link>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /#modalBox -->
+    </div><!-- /.container-fluid -->
 </template>
 
 <script>
