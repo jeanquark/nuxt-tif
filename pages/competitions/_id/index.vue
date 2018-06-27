f0d1b57db8cd93c5017693cd904a7badb27353fd
Macgama <33863329+macgama@users.noreply.github.com>
2018-06-27 10:51:04 +0200

    Update index.vue

--- pages/competitions/_id/index.vue
+++ pages/competitions/_id/index.vue
@@ -19,17 +19,17 @@
 							<h1>Supporter une nouvelle équipe ?</h1>
 						</div>
 						<div class="flex-container-modal-Title banner text-center">
-							<h2>{{ loadedCompetition.category.name }}</br>{{ loadedCompetition.name }}</br>{{ loadedCompetition.countries.name }}</h2>
+							<h2>{{ loadedCompetition.category.name }}</br>{{ loadedCompetition.name }}</br>{{ loadedCompetition.countries }}</h2>
 						</div>
 						<div class="flex-container-modal-OtherTeam">
 							<h6>Choisis ton équipe !</h6>
 						</div>
                         <div class="flex-container-modal-OtherTeam-Img">
-                            <div class="OtherTeam"  v-for="team in loadedTeamsByCompetition" style="cursor: pointer;" :class="{active: selectedTeams.findIndex(e => e.id === team.id) != -1}" @click="selectTeam(team)">
-                                <img :src="'/images/teams/' + team.image" class="imgModalAvatar" v-bind:class="{active: isActive}" /><nuxt-link :to="'/teams/' + team.slug" class="overlayOtherTeam"><div class="textActivity" v-if="team">{{ team.category.name }}</br>{{ team.name }}</br>+Infos</div></nuxt-link>
+                            <div class="OtherTeam" v-for="team in loadedTeamsByCompetition" style="cursor: pointer;" :class="{active: selectedTeams.findIndex(e => e.id === team.id) != -1}" @click="selectTeam(team)">
+                                <img :src="'/images/teams/' + team.image" class="imgModalAvatar" v-bind:class="{active: isActive}" /><div class="textActivity" v-if="team">{{ team.category.name }}</br>{{ team.name }}</br>+Infos</div>
                             </div>
                         </div>
-                    </div><!-- /#modalBoxContent -->
+                    </div><!-- /#modalBoxContent -->	
 
                     <!-- Modal footer -->
                     <div class="modal-footer">
@@ -84,10 +84,66 @@
                     }
                 })
                 return teams
-            },
-            loadedUserTeams () {
-                return this.$store.getters['users/loadedUserTeams']
-            }
+           },
+           loadedUserTeams () {
+                // return this.selectedTeams.push({id: '-LBVgvOsCUALzowK576H', name: 'Argentina'}, {id: '-LBVgvOxY6A_ylDQBOmp', name: 'Australia'}, {id: '-LBVgvR__QJEcYxQ7a7g'})
+                // return this.$store.getters['users/loadedUserTeams']
+                // const abc = [{id: '-LBVgvOsCUALzowK576H'}, {id: '-LBVgvOxY6A_ylDQBOmp'}, {id: '-LBVgvR__QJEcYxQ7a7g'}]
+                // return abc
+                // const userTeams = []
+                // 1) First retrieve all userteam ids
+                const teamIds = this.$store.getters['users/loadedUserTeams']
+                // return teamIds
+                console.log(teamIds)
+                // return teamIds
+
+                // 2) Then get complete team data
+                const teams = []
+                teamIds.forEach((teamId) => {
+                    console.log(teamId)
+                    let abc = this.loadedTeamsByCompetition.find(team => team.id === teamId)
+                    teams.push(abc)
+                })
+                console.log(teams)
+                // for (let teamId in teamIds) {
+                //     console.log(teamId)
+                //     if (teamId.hasOwnProperty()) {
+                //         console.log('abc')
+                //         console.log(lunch[key]); // value (ex. turkey)
+                //     }
+                // }
+                // console.log(teams)
+                // return
+                // console.log(teamIds)
+                // this.loadedTeamsByCompetition.forEach((team) => {
+                //     if (teamIds.find(el => el === team.id)) {
+                //         userTeams.push({id: team.id, name: team.name})
+                //     }
+                // })
+                // console.log(userTeams)
+                // return this.$store.getters['users/loadedUserTeams']
+                // console.log(this.loadedTeamsByCompetition)
+                // let def = this.loadedTeamsByCompetition.filter(el => el.id === '-LBVgvOsCUALzowK576H')
+                // let ghi = teamIds.some(el => this.loadedTeamsByCompetition.includes(el))
+                // console.log(ghi)
+                let def = teams.map(obj => {
+                    let rObj = {}
+                    rObj['id'] = obj.id
+                    rObj['name'] = obj.name
+                    this.selectedTeams.push(rObj)
+                    return rObj
+                })
+                console.log(def)
+                // this.selectedTeams.push(def)
+               return def
+              const abc = [{id: '-LBVgvOsCUALzowK576H', name: 'Argentina'}, {id: '-LBVgvOxY6A_ylDQBOmp', name: 'Australia'}, {id: '-LBVgvR__QJEcYxQ7a7g', name: 'Republic of Korea'}]
+              // this.selectedTeams.push(abc)
+                return abc
+
+           },
+           abc () {
+                return this.selectedTeams.push[{id: '-LBVgvOsCUALzowK576H', name: 'Argentina'}, {id: '-LBVgvOxY6A_ylDQBOmp', name: 'Australia'}, {id: '-LBVgvR__QJEcYxQ7a7g', name: 'Republic of Korea'}]
+             }
         },
         methods: {
             selectTeam (team) {
