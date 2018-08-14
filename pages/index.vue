b593e888ad2e7a556878a1cf9aa3af2474a551c2
Macgama <33863329+macgama@users.noreply.github.com>
2018-08-14 13:05:33 +0200

    Add files index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -91,6 +91,8 @@
                     <nuxt-link :to="localePath({ name: 'register'})"><button title="Inscription" class="button stroke smoothscroll">{{ $t('pages.index.sign_up') }}</button></nuxt-link>           
                     <!-- <nuxt-link to="/login"><button title="Connection" class="button button-primary">{{ $t('pages.index.log_in') }}</button></nuxt-link>     -->
                     <nuxt-link :to="localePath({ name: 'login'})"><button title="Connection" class="button button-primary">{{ $t('pages.index.log_in') }}</button></nuxt-link>
+					<button type="submit" class="button btn-facebook" @click.prevent="signInWithFacebook" :disabled="loading"><i class="fa fa-facebook"></i> {{ $t('pages.login.facebook_oauth') }}</button>
+					<button type="submit" class="button btn-google" @click.prevent="signInWithGoogle"><i class="fa fa-google"></i> {{ $t('pages.login.google_oauth') }}</button>
                 </div>          
             </div>
 
@@ -702,22 +704,42 @@
                 console.log('onClickChild')
                 location.hash = "#" + this.anchors[n];
             },
-            async signUserUp () {
-                console.log('signUserUp')
-                await this.$store.dispatch('users/signUserUp', {
-                    email: this.email,
-                    password: this.password,
-                    password_confirm: this.password_confirm
-                })
-                this.$router.replace('/home')
-            },
             async signUserIn () {
                 console.log('signUserIn')
-                await this.$store.dispatch('users/signUserIn', {
+                await this.$store.dispatch('firebase-auth/signUserIn', {
                     email: this.email,
                     password: this.password
                 })
-                this.$router.replace('/home')
+                if (this.$i18n.locale != 'en') {
+                    console.log('done')
+                    this.$router.replace('/' + this.$i18n.locale + '/home')
+                } else {
+                    console.log('done')
+                    this.$router.replace('/home')
+                }
+            },
+            async signInWithGoogle () {
+                console.log('signInWithGoogle')
+                await this.$store.dispatch('firebase-auth/signInWithGooglePopup')
+                if (this.$i18n.locale != 'en') {
+                    console.log('done')
+                    this.$router.replace('/' + this.$i18n.locale + '/home')
+                } else {
+                    console.log('done')
+                    this.$router.replace('/home')
+                }
+            },
+            async signInWithFacebook () {
+                console.log('signInWithFacebook')
+                return this.$store.dispatch('firebase-auth/signInWithFacebookPopup').then(() => {
+                    if (this.$i18n.locale != 'en') {
+                        console.log('done')
+                        this.$router.replace('/' + this.$i18n.locale + '/home')
+                    } else {
+                        console.log('done')
+                        this.$router.replace('/home')
+                    }
+                })
             },
             sendEmail () {
                 this.$store.commit('setLoading', true, { root: true })
@@ -750,6 +772,9 @@
 </script>
 
 <style scoped>
+	html {
+		font-size: 10px;
+	}
     .active {
         /*border: 1px solid orangered;*/
     }
@@ -758,5 +783,61 @@
     }
     .error {
         color: red;
+    }
+    input[type="text"] {
+        font-family: 'Comic Sans MS';
+        font-weight: 900;
+        font-style: italic;
+        font-stretch: ultra-condensed;
+    }
+    input[type="text"]::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+        font-family: 'bangers';
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+    }
+    input[type="text"]::-moz-placeholder { /* Firefox 19+ */
+        font-family: 'bangers';
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+    }
+    input[type="text"]:-ms-input-placeholder { /* IE 10+ */
+        font-family: 'bangers';
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+    }
+    input[type="text"]:-moz-placeholder { /* Firefox 18- */
+        font-family: 'bangers';
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+    }
+    .button-primary {
+        color: #fff;
+    }
+    .button-primary:hover {
+        background-color: #000;
+    }
+    .button-primary:disabled {
+        background-color: #d8d8d8;
+        color: #fff;
+    }
+    .btn-facebook {
+        background-color: #3c5a99;
+        color: #fff;
+    }
+    .btn-facebook:hover {
+        background-color: #6180c1;
+        cursor: pointer;
+    }
+    .btn-google {
+        background-color: #d6492f;
+        color: #fff;
+    }
+    .btn-google:hover {
+        background-color: #e3816f;
+        cursor: pointer;
     }
 </style>
