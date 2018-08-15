dd142fb956022bc9bd46b6cb1c73941efc90b8a7
Macgama <33863329+macgama@users.noreply.github.com>
2018-08-15 08:16:38 +0200

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
             </div> 
         </header> -->  
 
-        <!--<quick-menu :menu-count="count" :icon-class="icons" :backgroundColor="color" @clicked="onClickChild" style="cursor: pointer; z-index: 10;"></quick-menu>-->
+        <quick-menu :menu-count="count" :icon-class="icons" :backgroundColor="color" @clicked="onClickChild" style="cursor: pointer; z-index: 10;"></quick-menu>
         
         <!-- intro section
         ================================================== -->
@@ -200,7 +200,7 @@
                         <div class="timeline-block">
 
                             <div class="timeline-ico">
-                                <i class="fa fa-dollar-sign fa-2x"></i>
+                                <i class="fa fa-dollar fa-2x"></i>
                             </div>
 
                             <div class="timeline-header">
