5acd867e3f3a1b0be5660991305ae366ffa5cd93
Macgama <33863329+macgama@users.noreply.github.com>
2018-09-12 08:08:05 +0200

    Add files via upload

--- components/avatar/EyebrowsColor.vue
+++ components/avatar/EyebrowsColor.vue
@@ -1,25 +1,25 @@
 <template>
 	<div class="flex-container-modalAvatarImg">
-	    <div v-for="index in total_eyebflex-container-modalAvatarImgs_colors" @click="addToMerge({gender: gender, type: 'eyebflex-container-modalAvatarImgs', property: 'color', image: 'eyebflex-container-modalAvatarImgs' + eyebflex-container-modalAvatarImgs_shape + convertTo2Digits(index) + '.png', index: index, layerPosition: 4})">
-            <img :src="'/images/avatars/jm/' + gender + '/' + 'eyebflex-container-modalAvatarImgs' + '/' + 'colors' + '/eyebflex-container-modalAvatarImgsColor' + convertTo2Digits(index) + '.png'" class="imgModalAvatar" :class="{active: (eyebflex-container-modalAvatarImgs_color === convertTo2Digits(index)) }" style="cursor: pointer;" />
+	    <div v-for="index in total_eyebrows_colors" @click="addToMerge({gender: gender, type: 'eyebrows', property: 'color', image: 'eyebrows' + eyebrows_shape + convertTo2Digits(index) + '.png', index: index, layerPosition: 4})">
+            <img :src="'/images/avatars/jm/' + gender + '/' + 'eyebrows' + '/' + 'colors' + '/eyebrowsColor' + convertTo2Digits(index) + '.png'" class="imgModalAvatar" :class="{active: (eyebrows_color === convertTo2Digits(index)) }" style="cursor: pointer;" />
         </div>
 	</div>
 </template>
 
 <script>
 	export default {
-		props: ['gender', 'eyebflex-container-modalAvatarImgs'],
+		props: ['gender', 'eyebrows'],
 		data () {
 			return {
-				total_eyebflex-container-modalAvatarImgs_colors: 45,
+				total_eyebrows_colors: 45,
 			}
 		},
 		computed: {
-			eyebflex-container-modalAvatarImgs_shape () {
-				return this.eyebflex-container-modalAvatarImgs ? this.eyebflex-container-modalAvatarImgs.match(/\d+/)[0].substr(0, 2) : '01'
+			eyebrows_shape () {
+				return this.eyebrows ? this.eyebrows.match(/\d+/)[0].substr(0, 2) : '01'
 			},
-			eyebflex-container-modalAvatarImgs_color () {
-				return this.eyebflex-container-modalAvatarImgs ? this.eyebflex-container-modalAvatarImgs.match(/\d+/)[0].substr(2, 4) : '01'
+			eyebrows_color () {
+				return this.eyebrows ? this.eyebrows.match(/\d+/)[0].substr(2, 4) : '01'
 			}
 		},
 		methods: {
--- components/avatar/EyebrowsShape.vue
+++ components/avatar/EyebrowsShape.vue
@@ -1,28 +1,28 @@
 <template>
-	<div class="flex-container-modalAvatarImg">
-	    <div v-for="index in total_eyebflex-container-modalAvatarImgs_shapes" @click="addToMerge({gender: gender1, type: 'eyebflex-container-modalAvatarImgs', property: 'color', image: 'eyebflex-container-modalAvatarImgs' + convertTo2Digits(index) + eyebflex-container-modalAvatarImgs_color + '.png', index: index, layerPosition: 4})">
-            <img :src="'/images/avatars/jm/' + gender1 + '/' + 'eyebflex-container-modalAvatarImgs' + '/' + 'eyebflex-container-modalAvatarImgs' + convertTo2Digits(index) + '01' + '.png'" class="imgModalAvatar" :class="{active: (eyebflex-container-modalAvatarImgs_shape === convertTo2Digits(index)) }" style="cursor: pointer;" />
+	<div class="row">
+	    <div v-for="index in total_eyebrows_shapes" @click="addToMerge({gender: gender1, type: 'eyebrows', property: 'color', image: 'eyebrows' + convertTo2Digits(index) + eyebrows_color + '.png', index: index, layerPosition: 4})">
+            <img :src="'/images/avatars/jm/' + gender1 + '/' + 'eyebrows' + '/' + 'eyebrows' + convertTo2Digits(index) + '01' + '.png'" class="imgModalAvatar" :class="{active: (eyebrows_shape === convertTo2Digits(index)) }" style="cursor: pointer;" />
         </div>
 	</div>
 </template>
 
 <script>
 	export default {
-		props: ['gender', 'eyebflex-container-modalAvatarImgs'],
+		props: ['gender', 'eyebrows'],
 		data () {
 			return {
-				total_eyebflex-container-modalAvatarImgs_shapes: 17,
+				total_eyebrows_shapes: 17,
 			}
 		},
 		computed: {
 			gender1 () {
 				return this.gender
 			},
-			eyebflex-container-modalAvatarImgs_shape () {
-				return this.eyebflex-container-modalAvatarImgs ? this.eyebflex-container-modalAvatarImgs.match(/\d+/)[0].substr(0, 2) : '01'
+			eyebrows_shape () {
+				return this.eyebrows ? this.eyebrows.match(/\d+/)[0].substr(0, 2) : '01'
 			},
-			eyebflex-container-modalAvatarImgs_color () {
-				return this.eyebflex-container-modalAvatarImgs ? this.eyebflex-container-modalAvatarImgs.match(/\d+/)[0].substr(2, 4) : '01'
+			eyebrows_color () {
+				return this.eyebrows ? this.eyebrows.match(/\d+/)[0].substr(2, 4) : '01'
 			}
 		},
 		methods: {
