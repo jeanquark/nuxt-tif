c69cae41713b07ed677c3a06f7bdeed4062ca691
Macgama <33863329+macgama@users.noreply.github.com>
2018-09-14 07:05:00 +0200

    Add files via upload

--- components/loading.vue
+++ components/loading.vue
@@ -22,14 +22,12 @@ export default {
 
 <style scoped>
 .loading-page {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  height: 50%;
   background: rgba(255, 255, 255, 0.8);
   text-align: center;
-  padding-top: 200px;
   font-size: 30px;
   font-family: sans-serif;
 }
