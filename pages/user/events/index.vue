13298f2d64af2dfdc2515a0350c9508ca89ba5d9
Macgama <33863329+macgama@users.noreply.github.com>
2018-10-05 08:04:19 +0200

    Add files via upload

--- pages/user/events/index.vue
+++ pages/user/events/index.vue
@@ -107,36 +107,43 @@
 					</div>
 					<div class="flex-container-modal-Action">
 						<table class="table tableText">
-						  <tbody>
-							<tr class="borderResultat" v-for="event of events">
-							  	<td class="tdResultat1 text-left" style="">
-							  		<a href="autresEquipesDetails.html" class="linkEvent"><img v-lazy="'/images/teams/' + event.home_team.slug + '.png'" class="imgModalAgendaFlags"/> {{ event.home_team.name }}</a>
-							  		<transition name="fade" mode="out-in" :duration="{ enter: 3000, leave: 2000 }">
-							  			<span class="" style="float: right; padding: 15px;" :key="getHomeTeamScore(event.score)" v-if="event.status === 'IN PLAY' || event.status === 'FINISHED'">
-							  				{{ getHomeTeamScore(event.score) }}
-							  			</span>
-							  		</transition>
-							  	</td>
-							  	<td class="tdResultat text-center">
-							  		<nuxt-link :to="'/user/events/' + event.id" class="linkEvent">
-							  			Football</br>
-							  			<span v-if="event.competition">{{ event.competition.name }}<br /></span>
-							  			<span v-if="event.round">{{ event.round.name }}<br /></span>
-							  			{{ event.date }}<br />
-							  			{{ convertToLocaltime(event.timestamp) }}</br>
-							  			<span :class="[event.status === 'IN PLAY' ? 'in_play' : '']">{{ event.status }}</span>
-							  		</nuxt-link></td>
-							  	<td class="tdResultat1 text-right">
-							  		<transition name="fade" mode="out-in" :duration="{ enter: 3000, leave: 2000 }">
-							  			<span class="" style="float: left; padding: 15px;" :key="getVisitorTeamScore(event.score)" v-if="event.status === 'IN PLAY' || event.status === 'FINISHED'">
-							  				{{ getVisitorTeamScore(event.score) }}
-							  			</span>
-							  		</transition>
-							  		<a href="autresEquipesDetails.html" class="linkEvent">{{ event.visitor_team.name }} <img v-lazy="'/images/teams/' + event.visitor_team.slug + '.png'" class="imgModalAgendaFlags"/></a>
-							  	</td>
-							</tr>	
-							
-						  </tbody>
+						  	<tbody>
+								<tr class="borderResultat" v-for="event of userEvents">
+									<td class="tdScore text-left">
+										<nuxt-link :to="'/user/events/' " event.id" class="linkEvent">
+											{{ event.date }}</br>
+											{{ convertToLocaltime(event.timestamp) }}
+										</nuxt-link>
+									</td>
+								  	<td class="tdScore1 text-center">
+								  		<a href="mesEquipesDetails.html" class="linkEvent"><img v-lazy="'/images/teams/' + event.home_team.slug + '.png'" class="imgModalAgendaFlags"/> {{ event.home_team.name }}</a>
+								  	</td>
+								  	<td class="tdScore2 text-center">
+										<nuxt-link :to="'/user/events/' " event.id" class="linkEvent">
+											<transition name="fade" mode="out-in" :duration="{ enter: 3000, leave: 2000 }">
+												<span class="" style="float: right; padding: 15px;" :key="getHomeTeamScore(event.score)" v-if="event.status === 'IN PLAY' || event.status === 'FINISHED'">
+													{{ getHomeTeamScore(event.score) }}
+												</span>
+											</transition>
+											<span>-</br><span :class="[event.status === 'IN PLAY' ? 'in_play' : '']">{{ event.status }}</span></span>
+											<transition name="fade" mode="out-in" :duration="{ enter: 3000, leave: 2000 }">
+												<span class="" style="float: left; padding: 15px;" :key="getVisitorTeamScore(event.score)" v-if="event.status === 'IN PLAY' || event.status === 'FINISHED'">
+													{{ getVisitorTeamScore(event.score) }}
+												</span>
+											</transition>
+										</nuxt-link>
+								  	</td>
+								  	<td class="tdScore1 text-right" style="width: 30%;">
+								  		<a href="autresEquipesDetails.html" class="linkEvent">{{ event.visitor_team.name }} <img v-lazy="'/images/teams/' + event.visitor_team.slug + '.png'" class="imgModalAgendaFlags"/></a>
+								  	</td>
+									<td class="tdScore text-right">
+										<nuxt-link :to="'/user/events/' " event.id" class="linkEvent">
+								  			<span v-if="event.competition">{{ event.competition.name }}<br /></span>
+								  			<span v-if="event.round">{{ event.round.name }}<br /></span>
+										</nuxt-link>
+									</td>
+								</tr>	
+						  	</tbody>
 						</table>
 					</div>
 				</div>
